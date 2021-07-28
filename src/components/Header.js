18e010812fe8a3d59879b497c13d5e0564b1ac19
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2021-07-27 21:02:06 -0300

    Delete src directory

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = {
-    render() {
-      document.querySelector(".header").innerHTML  = `
-      <img class="image" src="./publico/images/icone-sacola.png" width="25" height="25">
-      <h1>Loja do Alifer</h1>
-      `;
-    } 
-  }  
-  export { Header };
