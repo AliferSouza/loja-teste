1f5f772f8512e836bba375daf5f046a8e32293a2
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2021-07-30 18:53:34 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
 import { Context } from './components/Context.js'
 
-const App = {
-   start() {      
-       Context.render()
-        }
 
+const navigator = document.querySelector('.navigation')
+
+document.querySelector('.toggle').onclick = function () {
+    this.classList.toggle('active');
+    navigator.classList.toggle('active');
+}
+
+           
+
+//Mover o Menu quando em tela grande
+
+const el = document.querySelector(".navigation");
+
+let isResizing = false;
+
+el.addEventListener("mousedown", mousedown);
+
+function mousedown(e) {
+    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mouseup", mouseup);
+
+    let prevX = e.clientX;
+    let prevY = e.clientY;
+
+    function mousemove(e) {
+        if (!isResizing) {
+            let newX = prevX - e.clientX;
+            let newY = prevY - e.clientY;
+
+            const rect = el.getBoundingClientRect();
+
+            el.style.left = rect.left - newX + "px";
+            el.style.top = rect.top - newY + "px";
+
+            prevX = e.clientX;
+            prevY = e.clientY;
+        }
     }
 
+    function mouseup() {
+        window.removeEventListener("mousemove", mousemove);
+        window.removeEventListener("mouseup", mouseup);
+    }
+}
+
+
+
+
+
+const App = {
+    start() {
+        Context.render()
+    }
+
+}
+
+
+
+
 export { App }
+
--- src/components/Show.js
+++ src/components/Show.js
@@ -2,12 +2,12 @@ const Show = {
     renderCards(posts) {
        let output = ''
         for (let post of posts) {
-            output += `<div class="card">
+            output += `<div class="card ">
             <img src="${post.src}" width="70" height="70">
             <h2>${post.Titulo}</h2>
             <br>
             <br>
-            <a class="btn" href="${post.Link}">Adquirir</a>
+            <a class="btn" href="${post.Link}">Adquerir</a>
         </div>`
         }
         document.querySelector('.conteiner-cards').innerHTML = output
@@ -15,13 +15,13 @@ const Show = {
     carCards(cCards) {
         let output = ''
         for (let cCard of cCards) {
-            output += ` <div class="rect-card">
+            output += ` <div class="rect-card ">
             <img src="${cCard.src}" width="90" height="90">
             <div class="box-info">
                 <h2>${cCard.Titulo}</h2>
                 <br>
                 <br>            
-                <a class="btn-car" href="${cCard.Link}">Adquirir</a>
+                <a class="btn-car" href="${cCard.Link}">Adquerir</a>
             </div>
         </div>
     `
